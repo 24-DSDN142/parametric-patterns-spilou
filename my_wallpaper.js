62b58e57b8325cf53bf262e99c8e668d5ad7e73a
spilou <169110457+spilou@users.noreply.github.com>
2024-07-25 12:14:38 +1200

    Figuring out variables and if statements

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,7 +1,7 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let size = 90;
 
+let something = true
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -11,13 +11,40 @@ function setup_wallpaper(pWallpaper) {
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 0;
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(110, 146, 212);
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+
+rect(size, size, size, size);
+
+if(size > 50){
+fill(115, 54, 54);
 }
+else{
+fill(183, 79, 209);
+}
+
+ellipse(100, 100, size/2, size);
+
+}
+
+// strokeWeight(0);
+// fill(34, 73, 143);
+// rect(70, 20, 110, 150);
+
+// strokeWeight(1);
+// stroke(20, 44, 87);
+// rect(72, 22, 106, 146);
+
+// strokeWeight(0);
+// fill(255, 255, 255);
+// rect(10, 110, 125, 80);
+
+// strokeWeight(1);
+// stroke(191, 191, 191);
+// rect(12, 112, 121, 76);
