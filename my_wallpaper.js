8daab9d4121e0b25da61bcd9fa9470cc3f902333
spilou <169110457+spilou@users.noreply.github.com>
2024-07-31 17:25:47 +1200

    finished the tiger

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,13 +1,14 @@
 //your parameter variables go here!
-let face = 150;
-let nose = 100;
-let ear = 50;
-let innerear = 25;
+let face = 150; //changes the size of the main part of the face
+let nose = 100; //changes the size of the nose
+let ear = 50; //changes the size of the outer ears
+let innerear = 25; //changes the size of the inner ears
+let mouth = 100 //changes the y poition of the mouth line
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GLIDE_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -21,66 +22,58 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-//ears
-fill(166, 81, 7);
+//ears - outter ear
+strokeWeight(1);
+if(face > 100){
+  fill(166, 81, 7);
+}
+else{
+  fill(128, 90, 24);
+}
 ellipse(35, 50, ear, ear);
 ellipse(165, 50, ear, ear);
 
+//ears - inner ear
 fill(176, 176, 176);
 ellipse(35, 50, innerear, innerear);
 ellipse(165, 50, innerear, innerear);
 
-//face
-fill(166, 81, 7);
+//face - main part
+if(face > 100){
+  fill(166, 81, 7);
+}
+else{
+  fill(128, 90, 24);
+}
 ellipse(100, 100, face, face);
 
+//face - around eye patch
 fill(176, 176, 176);
-arc(100, 85, 80, 110, 180, 360, CHORD);
+arc(100, 90, 80, 110, 180, 360, CHORD);
 
+//stripes - left
 fill(0, 0, 0);
-ellipse(75, 70, 10, 20);
-ellipse(125, 70, 10, 20);
+triangle(26, 88, 26, 100, 45, 95);
+triangle(27, 117, 26, 104, 45, 105);
 
-//nose
-fill(176, 176, 176);
-ellipse(100, 120, nose, nose);
-
-fill(0, 0, 0);
-arc(100, 100, 90, 60, 180, 360, CHORD);
-
-line(100, 100, 100, 125);
-
-}
-
-// strokeWeight(0);
-// fill(34, 73, 143);
-// rect(70, 20, 110, 150);
-
-// strokeWeight(1);
-// stroke(20, 44, 87);
-// rect(72, 22, 106, 146);
-
-// strokeWeight(0);
-// fill(255, 255, 255);
-// rect(10, 110, 125, 80);
-
-// strokeWeight(1);
-// stroke(191, 191, 191);
-// rect(12, 112, 121, 76);
-
-// if(nose > 50){
-//   fill(199, 199, 199);
-//   }
-//   else{
-//   fill(28, 28, 28);
-//   }
-
+//stripes - right
+triangle(174, 112, 174, 100, 155, 105);
+triangle(173, 83, 174, 96, 155, 95);
 
 //eyes
-// fill(232, 232, 232);
-// arc(75, 90, 20, 80, 180, 360, CHORD);
-// arc(125, 90, 20, 80, 180, 360, CHORD);
+ellipse(80, 68, 10, 20);
+ellipse(120, 68, 10, 20);
 
+//nose - main part
+fill(150, 150, 150);
+ellipse(100, 120, nose, nose);
 
-// arc(75, 90, 15, 70, 180, 360, CHORD);
-// arc(125, 90, 15, 70, 180, 360, CHORD);
+//nose - black part
+fill(0, 0, 0);
+ellipse(100, 95, nose, 30);
+
+//mouth - line
+strokeWeight(2);
+line(100, mouth, 100, 125);
+
+}
