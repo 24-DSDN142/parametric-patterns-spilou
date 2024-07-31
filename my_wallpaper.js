46345804ef5598c85f131cfcf4b85bd56aa6ad0d
spilou <169110457+spilou@users.noreply.github.com>
2024-07-31 19:23:13 +1200

    added background and made all wallpapers

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -7,13 +7,13 @@ let mouth = 100 //changes the y poition of the mouth line
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GLIDE_WALLPAPER);
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.resolution(A3);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 0;
+  pWallpaper.grid_settings.row_offset  = 100;
 }
 
 function wallpaper_background() {
@@ -22,41 +22,66 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
-//ears - outter ear
+//lines in the background
+strokeWeight(5);
+
+//black lines
+stroke(0, 0, 0);
+
+line(0, 0, 0, 200);
+line(100, 0, 100, 200);
+line(200, 0, 200, 200);
+
+//dark oange lines
+stroke(84, 27, 0);
+
+line(50, 0, 50, 200);
+line(150, 0, 150, 200);
+
+//returning the stroke colour to black
+stroke(0, 0, 0);
+
+//outter ears, with if statement to change colour of ears if the head size is smaller than 100
 strokeWeight(1);
+
 if(face > 100){
   fill(166, 81, 7);
 }
 else{
   fill(128, 90, 24);
 }
+
 ellipse(35, 50, ear, ear);
 ellipse(165, 50, ear, ear);
 
-//ears - inner ear
-fill(176, 176, 176);
+//inner ears
+fill(212, 212, 212);
+
 ellipse(35, 50, innerear, innerear);
 ellipse(165, 50, innerear, innerear);
 
-//face - main part
+//main part of the face, with if statement to change the face's colour if it is smaller than 100
 if(face > 100){
   fill(166, 81, 7);
 }
 else{
   fill(128, 90, 24);
 }
+
 ellipse(100, 100, face, face);
 
-//face - around eye patch
-fill(176, 176, 176);
+//patch of white around the eye
+fill(212, 212, 212);
+
 arc(100, 90, 80, 110, 180, 360, CHORD);
 
-//stripes - left
+//stripes on the left hand side
 fill(0, 0, 0);
+
 triangle(26, 88, 26, 100, 45, 95);
 triangle(27, 117, 26, 104, 45, 105);
 
-//stripes - right
+//stripes on the right hand side
 triangle(174, 112, 174, 100, 155, 105);
 triangle(173, 83, 174, 96, 155, 95);
 
@@ -64,15 +89,15 @@ triangle(173, 83, 174, 96, 155, 95);
 ellipse(80, 68, 10, 20);
 ellipse(120, 68, 10, 20);
 
-//nose - main part
+//main part of the nose
 fill(150, 150, 150);
 ellipse(100, 120, nose, nose);
 
-//nose - black part
+//black part of the nose
 fill(0, 0, 0);
 ellipse(100, 95, nose, 30);
 
-//mouth - line
+//mouth line
 strokeWeight(2);
 line(100, mouth, 100, 125);
 
